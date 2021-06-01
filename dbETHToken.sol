f9d10b6963861ebc3bee1c1be590b7cd20df249e
vahid-dev <vahidgh.vgh@gmail.com>
2021-06-01 20:22:08 +0430

    Add dbETH

---
+++ dbETHToken.sol
@@ -0,0 +1,39 @@
+//Be name khoda
+
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/AccessControl.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract dbETHToken is ERC20, AccessControl{
+
+	uint256 public currentPointIndex = 0;
+
+	bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+	bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+	bytes32 public constant CURRENT_POINT_INDEX_SETTER_ROLE = keccak256("CURRENT_POINT_INDEX_SETTER_ROLE");
+
+	constructor() public ERC20("bonded ethereum", "dbETH") {
+		_setupRole(DEFAULT_ADMIN_ROLE, msg.sender);	
+	}
+
+	function mint(address to, uint256 amount) public {
+        // Check that the calling account has the minter role
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+        _mint(to, amount);
+    }
+
+	function burn(address from, uint256 amount) public {
+        require(hasRole(BURNER_ROLE, msg.sender), "Caller is not a burner");
+        _burn(from, amount);
+    }
+
+	function setCurrentPointIndex(uint256 _currentPointIndex) public {
+		require(hasRole(CURRENT_POINT_INDEX_SETTER_ROLE, msg.sender), "Caller is not a currentPointIndex setter");
+		currentPointIndex = _currentPointIndex;
+	}
+
+}
+//Dar panah khoda
